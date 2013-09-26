97681762f481595eaf875ec6f788deab307b0224
egidar <cristian.gidar@gmail.com>
2013-09-26 10:07:32 +0300

    add

--- eManager.Web/Global.asax.cs
+++ eManager.Web/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using eManager.Web.Infrastructure;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -23,6 +25,9 @@ namespace eManager.Web
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+
+            Database.SetInitializer<DepartmentDB>(new Devtalk.EF.CodeFirst.DontDropDbJustCreateTablesIfModelChanged<DepartmentDB>());
+
         }
     }
 }
